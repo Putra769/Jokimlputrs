8ffcc477d6d1d64492fc5e54f3d053937d8173c9
PutraOfficial028 <putnai72@gmail.com>
2025-11-19 23:19:02 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function orderNow(paket) {
-    const nomorWA = "628xxxxxxxxxx"; // GANTI KE NOMOR KAMU
-    const pesan = `Halo, saya ingin order joki ML untuk paket: ${paket}`;
-    const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
-
-    window.open(url, "_blank");
-}
